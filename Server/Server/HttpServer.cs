b26289cd11ec1afcf356d79a781405253866548e
DenisCo1100100 <kodec_den2001@mail.ru>
2021-09-25 05:45:23 +0300

    ADD: added server

---
+++ Server/Server/HttpServer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+	class HttpServer
+	{
+		HttpListener _httpListener;
+		
+		public HttpServer()
+		{
+			_httpListener = new HttpListener();
+			_httpListener.Prefixes.Add("http://localhost:8888/connection/");
+			_httpListener.Start();
+
+			Console.WriteLine("Waiting for a connection...");
+
+			HttpListenerContext context = _httpListener.GetContext();
+			HttpListenerRequest request = context.Request;
+			HttpListenerResponse response = context.Response;
+
+			string responseStr = "[SERVER] => Hello, i'm а server";
+			byte[] buffer = Encoding.UTF8.GetBytes(responseStr);
+			Stream output = response.OutputStream;
+			output.Write(buffer, 0, buffer.Length);
+			output.Close();
+
+			_httpListener.Stop();
+			Console.WriteLine("Connection processing is complete");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Server
+{
+	class Program
+	{
+		static async Task Main()
+		{
+			var httpServer = new HttpServer();
+
+			var client = new HttpClient();
+			Console.WriteLine("Connection client...");
+			var result = await client.GetAsync("http://localhost:8888/connection/");
+
+			Console.WriteLine(result);
+		}
+	}
+}
